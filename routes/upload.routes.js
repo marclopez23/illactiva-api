3a2d2934e7a1869ee6a7d70b79d93f7a1d703d52
Marc Lopez Pujol <marclopez@MacBook-Pro-de-Marc-2.local>
2021-03-28 21:05:28 +0200

    Done

--- routes/upload.routes.js
+++ routes/upload.routes.js
@@ -3,7 +3,6 @@ const route = express.Router();
 const fileParser = require("../config/cloudinary-setup.config");
 
 route.post("/", fileParser.single("image"), (req, res, next) => {
-  console.log("req.file", req.file);
   if (!req.file) {
     next(new Error("No file uploaded"));
     return;
